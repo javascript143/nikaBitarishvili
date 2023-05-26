4fb9f3f602bc8ef8970ccd0596c13259fd685849
nikaBitarishvili <nika.bitarishvili@gmail.com>
2023-05-26 23:02:06 +0400

    davasrule davaleba

---
+++ lesson3homework/script/shecdoma.js
@@ -0,0 +1,13 @@
+let action = "move";
+
+switch(action) {
+    case "move" :
+        alert("you're moving!");
+        break;
+    case "jump":
+        alert("you,re jumping!");
+    case "run":
+        alert("you,re running");
+    default:
+        alert("invalid action")
+}
