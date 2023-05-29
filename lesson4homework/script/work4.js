47866033aae088c09e6d6ef4921f72bbcc9a5c88
nikaBitarishvili <nika.bitarishvili@gmail.com>
2023-05-29 22:46:56 +0400

    davasrule

---
+++ lesson4homework/script/work4.js
@@ -0,0 +1,9 @@
+// შექმენით ფუნქცია, რომელიც მასზე მიწოდებულ სტრიქონს გადააქცევს მასივად. მაგ. stringToArray(testString) => თუ მივაწვდით სტრიქონს “Hello, World” => გამოიტანს მის ვერსიას მასივში. [“Hello”, “World”]
+
+
+
+
+let myString = "hello world"
+
+
+console.log(myString.split(" "))
